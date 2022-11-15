d648857903051d3392890d3b3e48657fa6e59f62
Nan Andrei David <andrei.nan03@e-uvt.ro>
2022-11-15 20:46:24 +0200

    Initial Commit

---
+++ src/main/java/com/example/jspring_gdscbasic/JspringGdscBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.jspring_gdscbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspringGdscBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspringGdscBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jspring_gdscbasic/JspringGdscBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jspring_gdscbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspringGdscBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
